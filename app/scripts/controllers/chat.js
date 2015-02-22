64d7dca90d45b7a79586ab462a2df16c285a8623
Nic Nash <nicnash08@gmail.com>
2015-02-21 22:50:01 -0600

    small fixes/ changes.  need to fix user name on index.html still

--- app/scripts/controllers/chat.js
+++ app/scripts/controllers/chat.js
@@ -7,12 +7,11 @@
  * A demo of using AngularFire to manage a synchronized list.
  */
 angular.module('votingWithFireApp')
-  .controller('ChatCtrl', function ($scope, fbutil, user, $timeout) {
+  .controller('ChatCtrl', function ($scope, fbutil, $timeout) {
     // synchronize a read-only, synchronized array of messages, limit to most recent 10
     $scope.messages = fbutil.syncArray('messages', {limitToLast: 10});
-    $scope.user = user;
+    // $scope.user = simpleLogin.getUser();
 
-    console.log($scope.user);
     // display any errors
     $scope.messages.$loaded().catch(alert);
 
@@ -20,7 +19,7 @@ angular.module('votingWithFireApp')
     $scope.addMessage = function(newMessage) {
       if( newMessage ) {
         // push a message to the end of the array
-        $scope.messages.$add({name:user.twitter.username,text: newMessage})
+        $scope.messages.$add({name:$scope.userName,text: newMessage})
           // display any errors
           .catch(alert);
       }
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,8 +8,12 @@
  * Controller of the votingWithFireApp
  */
 angular.module('votingWithFireApp')
-  .controller('MainCtrl', function ($scope,$rootScope,$location, $route) {
+  .controller('MainCtrl', function ($scope,$rootScope,$location,simpleLogin,$route) {
     // $rootScope.message = "hello world#{#{$@#{@#{$#{@$@#{$@#{$@#{$@#{$@#{$@#{$}}}}}}}}}}}";
     $rootScope.$route = $route;
+    $scope.user = simpleLogin.getUser();
+    console.log($scope.user);
+    $rootScope.userName = $scope.user.twitter.username;
+
     // $rootScope.loc = $location.path();
   });
